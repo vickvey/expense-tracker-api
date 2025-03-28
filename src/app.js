775db10edaf85995708b6cd1565c4d0002229679
vickvey <vickvey0303@gmail.com>
2025-03-28 17:59:06 +0530

    added README.md and removed SSR

--- src/app.js
+++ src/app.js
@@ -9,24 +9,12 @@ import cookieParser from "cookie-parser";
 import authMiddleware from "./middlewares/authMiddleware.js";
 import expenseRouter from "./routes/expenseRouter.js";
 
-// Setting up EJS as the view engine for SSR
-import path from "path";
-import { fileURLToPath } from "url";
-
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET is not set in environment variables!");
 }
 
 const app = express();
-
-// Get the current directory path using import.meta.url
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Set EJS as the view engine
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 
 // Middlewares
 app.use(cors());
@@ -42,12 +30,6 @@ app.use("/auth", authRouter);
 
 // Protected Routes
 app.use("/expense", authMiddleware, expenseRouter);
-
-// Example SSR route (Home Page)
-app.get("/home", async (req, res) => {
-  res.render("home");
-});
-
 app.use("*", (req, res) => {
   ApiResponse.error(res, 404, "Invalid Request");
 });
